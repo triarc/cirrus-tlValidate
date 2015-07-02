f67fddec514e2c5ec9b7c38f9e0cf44fe5cdbbcc
Marco Bachmann <mb@triarc-labs.com>
2015-07-02 11:10:57 +0200

    Bumped version to 0.0.9

--- tlValidate.js
+++ tlValidate.js
@@ -241,18 +241,25 @@ mod.directive('tlRequiredAny', function () {
         }
     };
 });
-mod.directive('tlOnlyNum', function () { return function (scope, element, attrs) {
-    var keyCode = [8, 9, 37, 39, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 110];
-    element.bind("keydown", function (event) {
-        console.log($.inArray(event.which, keyCode));
-        if ($.inArray(event.which, keyCode) == -1) {
-            scope.$apply(function () {
-                scope.$eval(attrs.onlyNum);
-                event.preventDefault();
-            });
-            event.preventDefault();
+mod.directive('tlOnlyNum', function () { return function () {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function (scope, element, attr, ctrl) {
+            function inputValue(val) {
+                if (val) {
+                    var digits = val.replace(/[^0-9.,]/g, '');
+                    if (digits !== val) {
+                        ctrl.$setViewValue(digits);
+                        ctrl.$render();
+                    }
+                    return parseFloat(digits);
+                }
+                return undefined;
+            }
+            ctrl.$parsers.push(inputValue);
         }
-    });
+    };
 }; });
 var INTEGER_REGEXP = /^\-?\d+$/;
 mod.directive('tlSmartInteger', function () {
